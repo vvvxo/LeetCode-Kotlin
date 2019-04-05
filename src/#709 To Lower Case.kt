2eb69efe533343c3674d546b479f69af29b5d6c2
Kuiooo <lmmlobb@gmail.com>
2019-04-05 13:38:18 +0800

    Add 709

---
+++ src/#709 To Lower Case.kt
@@ -0,0 +1,11 @@
+fun toLowerCase(str: String): String {
+    var tmpStr = str.toCharArray()
+    for(i in tmpStr.indices){
+        if (tmpStr[i] in 'A'..'Z'){
+            tmpStr[i] = (tmpStr[i] + 32)
+        }
+    }
+    return String(tmpStr)
+}
+
+
